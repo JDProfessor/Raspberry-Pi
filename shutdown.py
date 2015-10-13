071a89ee40d1a2aa12a0c6981cc7b26bc01b0ae1
JD <91xiaodian@sina.com>
2015-10-13 11:09:05 +0800

    add led light

--- shutdown.py
+++ shutdown.py
@@ -7,15 +7,22 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 shutdown_btn = 18  #GPIO.1
-pulldown_btn = 23  #GPIO.4
+pulldown = 23      #GPIO.4
+led = 24       	   #GPIO.5
 
 GPIO.setup(shutdown_btn,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) #pull down shutdown_btn
-GPIO.setup(pulldown_btn,GPIO.OUT)
-GPIO.output(pulldown_btn, 0)
+GPIO.setup(pulldown,GPIO.OUT) #set pulldown out 0
+GPIO.output(pulldown, 0)
+GPIO.setup(led,GPIO.OUT)      #set led out 1
+GPIO.output(led, 1)
 
 def onPress(channel):
 	os.system("poweroff")
-	time.sleep(0.5)
+	for i in range (3):
+		GPIO.output(led, 0)
+		time.sleep(0.1)
+		GPIO.output(led, 1)
+		time.sleep(0.1)
 	GPIO.cleanup()
 
 GPIO.add_event_detect(shutdown_btn,GPIO.RISING,callback= onPress,bouncetime=500)
